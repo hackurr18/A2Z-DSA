98567b9b94f07d52fbc2998530047cec178e0e5b
KAPIL RAJPOOT <kapilrajpoot@KAPILs-MacBook-Air.local>
2026-08-16 02:56:56 +0530

    optimal approach

---
+++ c++/largestofarr/slargest2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int slargest(int arr[],int n){
+    int largest=arr[0];
+    int slargest= -1;
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            slargest=largest;
+            largest=arr[i];
+            
+        } 
+
+        if(arr[i]>slargest && arr[i]!=largest) slargest=arr[i];
+    }
+    return slargest;
+}
+int main(){
+    int arr[6]={1,2,4,7,7,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<n<<endl;
+    cout<<slargest(arr,n);
+    return 0;
+}
