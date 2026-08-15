25cfd6a1cc3c497df669661e138e4916dc880772
KAPIL RAJPOOT <kapilrajpoot@KAPILs-MacBook-Air.local>
2026-08-16 01:32:17 +0530

    better approach

---
+++ c++/slargest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[6]={2,6,6,5,9,9};
+    int largest=arr[0];
+    for(int i=1;i<6;i++){
+        if(largest<arr[i]) largest = arr[i];
+    }
+    int slargest=-1;
+    for(int i=0;i<6;i++){
+        if(arr[i]>slargest && arr[i]!=largest) slargest=arr[i];
+    }
+    cout<<slargest;
+    return 0;
+}
