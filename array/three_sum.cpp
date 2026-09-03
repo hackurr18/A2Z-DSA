b8c2ef05ead05e6cfca1625b19163ffefb2fb970
KAPIL RAJPOOT <kapilrajpoot549@gmail.com>
2026-09-03 23:37:45 +0530

    update:optimal appproach

--- array/three_sum.cpp
+++ array/three_sum.cpp
@@ -39,9 +39,38 @@ vector<vector<int>> threeSum(vector<int>&nums){
     vector<vector<int>>ans(st.begin(),st.end());
     return ans;
 }
+//optimal tc O(nlogn )+ O(n*n)
+vector<vector<int>> threeSumm(vector<int>&nums){
+    int n=nums.size();
+    vector<vector<int>>ans;
+    sort(nums.begin(),nums.end()); //nlogn 
+
+    for(int i=0;i<n;i++){ //O(n)
+        // Skip duplicates for the first element
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        int j=i+1 ,k=n-1;
+        while(j<k){ //O(n)
+            if(nums[i]+nums[j]+nums[k]<0){
+                j++ ;
+            }
+            else if(nums[i]+nums[j]+nums[k]>0){
+                k--;
+            }
+            else{
+                vector<int>temp={nums[i],nums[j],nums[k]};
+                j++, k-- ;
+                ans.push_back(temp);
+                while(j<k && nums[j]==nums[j-1]) j++;
+                while(j<k && nums[k]==nums[k+1]) k--;
+            }
+        }
+    }    
+    return ans;
+}
 int main(){
-    vector<int>nums={-1,0,1,2,-1,-4};
-    vector<vector<int>>ans=threeSum(nums);
+    //vector<int>nums={-1,0,1,2,-1,-4};
+    vector<int>nums={2,-1,2,0,-2,-1,0,-2,2,0,2,-1,-2};
+    vector<vector<int>>ans=threeSumm(nums);
     for(const auto &i:ans){
         for(auto x:i){
             cout<<x<<" ";
