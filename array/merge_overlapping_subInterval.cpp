308e82d8eb75260a9bbb93db31b0205459e804f5
KAPIL RAJPOOT <kapilrajpoot549@gmail.com>
2026-09-06 16:57:10 +0530

    update:optimal

---
+++ array/merge_overlapping_subInterval.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    int n=interval.size();
+        
+    }
+int main(){
+    vector<vector<int>>intervals={{1, 3},{2, 6},{2, 4}, {8, 9}, {9, 11}, {8, 10}, {15, 18}, {16, 17}};
+    merge(intervals);
+    return 0;
+}
--- array/subarray_XOR_K.cpp
+++ array/subarray_XOR_K.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<stack>
+#include<map>
 using namespace std;
 //better O(N^2)
 int county_subarray(vector<int>nums,int target){
@@ -14,7 +14,22 @@ int county_subarray(vector<int>nums,int target){
     }
     return cnt;
 }
-
+//optimal O(n) ||o(n log n) sc O(n)
+int count_subarray(vector<int>nums,int target){
+    int xr=0 , cnt=0;
+    map<int,int>mpp;
+    mpp[xr]++; //{0,1}
+    int n=nums.size();
+    
+    for(int i=0;i<n;i++){
+        xr=xr ^ nums[i];
+        int x=xr ^ target; // k
+        cnt= cnt + mpp[x];
+        mpp[xr]++;
+        
+    }
+    return cnt;
+}
 int main(){
     vector<int>nums={4,2,2,6,4};
     int target = 6;
