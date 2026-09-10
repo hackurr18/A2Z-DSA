1bc26cb09ac65a0d1852da10abe5695d9f2e16e7
KAPIL RAJPOOT <kapilrajpoot549@gmail.com>
2026-09-10 22:12:30 +0530

    find  x in array

---
+++ binarySearch/lower,upper_bound.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+using namespace std;
+//lowerbound
+//lb=lower_bound(nums.begin(),nums.end(),x)-nums.begin();
+//tc O(logn)
+int lower(vector<int>&nums, int target){
+    int n=nums.size();
+    int ans=n;
+    int low=0, high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]>=target){
+            ans=mid;
+            high=mid-1;
+        }
+        else low=mid+1; 
+
+    }
+    return ans;
+}
+//upperbound
+int upper(vector<int>&nums,int target){
+    int n=nums.size();
+    int ans=n;
+    int low=0, high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]>target){
+            ans=mid;
+            high=mid-1;
+        }
+        else low=mid+1; 
+
+    }
+    return ans;
+}
+//search insert position 
+int searchInsert(vector<int>&nums,int target){
+    int n=nums.size();
+    int ans=n;
+    int low=0, high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]>=target){
+            ans=mid;
+            high=mid-1;
+        }
+        else low=mid+1; 
+
+    }
+    return ans;
+}
+//floor
+int findFloor(int arr[], int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= x) {
+                ans = arr[mid];     // Potential floor
+                low = mid + 1;      // Search right side
+            } else {
+                high = mid - 1;     // Search left side
+            }
+        }
+        return ans;
+}
+//ceil
+int findCeil(int arr[], int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = arr[mid];     // Potential ceil
+                high = mid - 1;     // Search left side
+            } else {
+                low = mid + 1;      // Search right side
+            }
+        }
+        return ans;
+    }
+int main(){
+    vector<int>nums={1,2,3,3,4,8,8,9,9};
+    int target=5;
+    //cout<<lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+    //cout<<upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+    //cout<<upper(nums,target);
+    //cout<<lower(nums,target);
+    cout<<searchInsert(nums,target);
+    return 0;
+}
