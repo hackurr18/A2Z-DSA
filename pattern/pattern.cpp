573e14cfab73323e0a93098daa49c6250d6cc213
KAPIL RAJPOOT <kapilrajpoot549@gmail.com>
2026-09-14 23:53:45 +0530

    pattern 3

--- pattern/pattern.cpp
+++ pattern/pattern.cpp
@@ -25,12 +25,25 @@ void pattern2(int N) {
             cout << endl;
         }
 }
+ void pattern3(int N) {
+        // Outer loop for rows
+        for (int i = 1; i <= N; i++) {
+            // Inner loop for columns
+            // Print numbers from 1 to i
+            for (int j = 1; j <= i; j++) {
+                cout << j << " ";
+            }
+            // Move to the next row
+            cout << endl;
+        }
+    }
 
 
 
 int main() {
     int N = 5;  // Set the size of the square (5x5)
     //pattern1(N); // Call the function to print the pattern
-    pattern2(N);
+    //pattern2(N);
+    pattern3(N);
     return 0;
 }
