e0df66c5c3300669b3de007a9a30d53c38edc679
KAPIL RAJPOOT <kapilrajpoot549@gmail.com>
2026-09-18 23:57:42 +0530

    mini largest sum of the split A subarr

---
+++ binarySearch/bs on ans/splitArray_painter.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+#include<numeric>
+//return the minimized largest sum of the split A subarray
+using namespace std;
+//optimal yc O(log sum-max)*O(N)
+int countStudents(vector<int>&arr ,int pages){
+    int students=1;
+    long long pagesStudent =0;
+    for(int i=0;i<arr.size();i++){
+        if(pagesStudent +arr[i] <=pages){
+            pagesStudent+=arr[i];
+        }
+        else{
+            students +=1;
+            pagesStudent=arr[i];
+        }
+    }
+    return students;
+}
+int largestSubarraySumMinimized(vector<int>&arr ,int n,int m){
+    if(m>n) return -1;
+    int low= *max_element(arr.begin(),arr.end());
+    int high= accumulate(arr.begin(),arr.end(),0);
+    while(low<=high){
+        int mid=low +(high-low)/2;
+        int students= countStudents(arr,mid);
+        if(students>m){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return low;
+}
+int main(){
+    vector<int>arr={25,46,28,49,24};
+    int n=arr.size();
+    int m=4;
+    cout<<largestSubarraySumMinimized(arr,n,m);
+    return 0;
+}
