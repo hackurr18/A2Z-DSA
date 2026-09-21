67fd312269d2cfae7ade66e04fdc15019b88ad0b
KAPIL RAJPOOT <kapilrajpoot549@gmail.com>
2026-09-22 01:26:03 +0530

    optimal:find median of two sorted array

--- binarySearch/bs on ans/median_of_two_arr.cpp
+++ binarySearch/bs on ans/median_of_two_arr.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 //brute sc O(N)
-int median (vector<int>&nums1,vector<int>&nums2){
+int mediannn (vector<int>&nums1,vector<int>&nums2){
     int n1=nums1.size();
     int n2=nums2.size();
     int n=n1+n2;
@@ -74,10 +74,39 @@ double mediann(vector<int>&nums1, vector<int>&nums2){
     
     
 }
+//optimal tc O(min(log n1,log n2))
+double  median(vector<int>&nums1, vector<int>&nums2){
+    int n1=nums1.size();
+    int n2=nums2.size();
+    int n=n1+n2;
+    if(n1>n2) return median(nums2,nums1);
+    
+    int low=0 , high=n1;
+    int left=(n1+n2+1)/2;//left side of median line
+    while(low<=high){
+        int mid1=(low+high)/2;
+        int mid2=(left-mid1);
+
+        int l1=INT_MIN,l2=INT_MIN;
+        int r1=INT_MAX,r2=INT_MAX;
+        if(mid1<n1) r1=nums1[mid1];
+        if(mid2<n2) r2=nums2[mid2];
+        if(mid1 -1 >=0) l1=nums1[mid1-1];
+        if(mid2-1>=0) l2=nums2[mid2-1];
+        if( l1<=r2 && l2<=r1){
+            if(n%2==1) return max(l1,l2);
+            return ((double)(max(l1,l2)+min(r1,r2)))/2.0;
+        }
+        else if(l1>r2) high=mid1-1;
+        else low =mid1+1;
+    
+    }
+    return 0;
+}
 int main(){
-    //vector<int>nums1={1,3,4,7,10,12};
-    vector<int>nums1={};
+    vector<int>nums1={1,3,4,7,10,12};
+    //vector<int>nums1={};
     vector<int>nums2={2,3,6,15};
-    cout<<mediann(nums1,nums2);
+    cout<<median(nums1,nums2);
     return 0;
 }
